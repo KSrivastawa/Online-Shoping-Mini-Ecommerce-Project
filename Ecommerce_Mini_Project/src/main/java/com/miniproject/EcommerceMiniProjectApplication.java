232c58f43af06df7df9c00228feb988c4f77e7a1
KSrivastawa <kt.ketansri@gmail.com>
2022-11-18 09:59:20 +0530

    Initiated

---
+++ Ecommerce_Mini_Project/src/main/java/com/miniproject/EcommerceMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMiniProjectApplication.class, args);
+	}
+
+}
